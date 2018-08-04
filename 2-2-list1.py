51b8d406a068e55c5a9270eb0d2816a9e6a38a19
papanosuke <cbf17819@nifty.com>
2018-08-04 12:05:07 +0900

    Changes to be committed: 	modified:   2-2-list1.py

--- 2-2-list1.py
+++ 2-2-list1.py
@@ -1,1 +1,37 @@
+import random
 #
+#  使う変数の宣言
+#  (player_hand,player_money,dealer_hand)
+#  (RANK,SUIT)
+#
+#  トランプデッキを作ってシャッフルする関数
+#  (make_deck)
+#
+#  手札の情報を表示する関数
+#  (print_player_hand,print_dealer_hand)
+#
+#  ポイントを換算する関数
+#  (get_point)
+#
+#  プレイヤーの思考パターンを記述する関数
+#  (player_op)
+#
+#  ディーラーの思考パターンを記述する関数
+#  (dealer_op)
+#
+#  勝敗判定とそれに伴って変化するチップを計算する関数
+#  (win_lose)
+#
+# turn = 1
+#
+#  デッキを作る
+while(player_money > 0):
+	#  変数の初期化
+	#  ベットする額
+	#
+	#
+	#
+	#
+	#
+	turn += 1
+print("ゲームオーバー")
