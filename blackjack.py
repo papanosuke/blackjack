e438dbcae6112104d052bf20fc45d194a9895664
papanosuke <cdf17819@nifty.com>
2018-08-05 13:53:42 +0900

    Changes to be committed: 	modified:   2-2-list1.py

--- blackjack.py
+++ blackjack.py
@@ -23,6 +23,19 @@ import random
 #  勝敗判定とそれに伴って変化するチップを計算する関数
 #  (win_lose)
 #
+#(RANK)=数字
+#(SUIT)=スート
+RANK,SUIT = 0,1
+def get_point(hand):
+    result = 0
+    for card in hand:
+        if card[RANK] > 10:
+            num = 10
+        else:
+            num = card[RANK]
+        result = result + num
+    return result
+#
 def make_deck():
     suits = ["S","H","D","C"]
     ranks = range(1,14)
@@ -33,13 +46,29 @@ def make_deck():
 def main():
     turn = 1
     player_money = 100
-    deck = make_deck()
-    print(deck)
 
 #  デッキを作る
     while(player_money > 0):
         print("ターン：",turn)
         print("所持金",player_money)
+#
+#   プレイヤーの手札格納用リスト
+        player_hand = []
+#   ディーラーの手札格納用リスト
+        dealer_hand = []
+#   デッキ作成
+        deck = make_deck()
+        #print(deck)
+        for i in range(2):
+    #   デッキからプレイヤーの手札へ
+            player_hand.append(deck.pop())
+    #   デッキからディーラーの手札へ
+            dealer_hand.append(deck.pop())
+        print(player_hand)
+        print(get_point(player_hand))
+        print(dealer_hand)
+        print(get_point(dealer_hand))
+
         turn += 1
         input("次のターンへ")
     print("ゲームオーバー")
